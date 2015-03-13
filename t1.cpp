741f0d5ac794fca0f61071bf9da0885025fcb8aa
nbatista <nbatista@inf.ufsm.br>
2015-03-13 15:42:15 -0300

    update

--- t1.cpp
+++ t1.cpp
@@ -2,6 +2,19 @@
 #include <string.h> 
 int main (void)
 {
-
+	int n = 0,size;
+	int achou = 0;
+	char nome[100], nome1[100]; 
+	char linha[100];
+	FILE* fp; 
+	
+	printf("Digite o nome a procurar: ");
+	scanf("%120s",nome);
+	/* abre arquivos para leitura */
+	fp = fopen("alunos.txt","rt");
+	if (fp == NULL) {
+		printf("Não foi possível abrir arquivo de entrada.\n");
+		return 1;
+	}
 		return 0;
 	}
