041b48ad3ef857456a5f19c0af7974599fc62a5a
nbatista <nbatista@inf.ufsm.br>
2015-03-16 22:26:48 -0300

    convertendo .cpp em .c

--- t1.c
+++ t1.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <string.h> 
-int main (void)
+int main (int argc, char *argv[])
 {
 	int i;
 	int achou = 0;
-	char nome[100], nome1[100]; 
+	char nome1[100]; 
 	char linha[100];
 	FILE* fp; 
-	
-	printf("Digite o nome a procurar: ");
-	scanf("%120s",nome);
-	/* abre arquivos para leitura */
+
+
 	fp = fopen("alunos.txt","rt");
 	if (fp == NULL) {
 		printf("Não foi possível abrir arquivo de entrada.\n");
 		return 1;
 	}
 		while (fgets(linha,100,fp) != NULL) {
-			if (strstr(linha,nome) != NULL) {
+			if (strstr(linha,argv[1]) != NULL) {
 				achou = 1;
 				if (achou){	
 			
@@ -25,7 +23,7 @@ int main (void)
 				FILE *arq;
 				arq = fopen("notas.txt", "r");
 				if(arq == NULL)
-					printf("Erro, nao foi possivel abrir o arquivo\n");
+					printf("Não foi possível abrir arquivo de entrada.\n");
 				else
 					while( (fscanf(arq,"%s %f %f \n", nome1, &nota1, &nota2))!=EOF )
 						if (strstr(linha,nome1) != NULL) {
