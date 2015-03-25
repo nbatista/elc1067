3f5dcb005f63d5876dec624e3cecacaecc9c2733
nbatista <nbatista@inf.ufsm.br>
2015-03-25 00:12:46 -0300

    reconstruindo t1

--- t2/t2.c
+++ t2/t2.c
@@ -19,28 +19,14 @@ int main (int argc, char *argv[])
 		while (fgets(linha,100,fp) != NULL) {
 			if (strstr(linha,argv[1]) != NULL) {
 				achou = 1;
-				if (achou){	
-			
-				float nota1, nota2;
-				FILE *arq;
-				arq = fopen("notas.txt", "r");
-				if(arq == NULL)
-					printf("Não foi possível abrir arquivo de entrada.\n");
-				else
-					while( (fscanf(arq,"%s %f %f \n", nome1, &nota1, &nota2))!=EOF )
-						if (strstr(linha,nome1) != NULL) {
-							if (achou){
-								printf("\n\n  %.2f      ",  (nota1+nota2)/2);
-									for(i=9;i<10;i++)
-									printf(" %s\n", &linha[i]);								
-								}
+				if (achou){
+					printf(" %s\n", &linha[i]);								
+					}
 						
-						}
-								
-				}	
 			}
-			
-		}
+								
+		}	
+
 		fclose(fp);
 		return 0;
 	}
